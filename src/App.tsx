e4556b5f4ea958f41b2feba0c98e67904a2ad735
Rick Porter <rick@dscvr.one>
2024-07-21 07:38:18 -0700

    fix: understanding aspect ratio

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,57 @@ import {
 } from "@dialectlabs/blinks";
 import '@dialectlabs/blinks/index.css';
 import './blink.css'
+import { CanvasClient } from '@dscvr-one/canvas-client-sdk';
+import * as base58 from 'bs58';
+
+
+const chainId = 'solana:101';
 
 class MyActionAdapter implements ActionAdapter {
+  canvasClient: CanvasClient;
+  constructor() {
+    this.canvasClient = new CanvasClient();
+    this.canvasClient.ready().then(() => {
+      console.log('Canvas client ready');
+    });
+
+  }
+
   connect = async (_context: ActionContext) => {
     try {
-      return "Connected successfully";
+      if (!this.canvasClient) {
+        throw new Error('Canvas client not initialized');
+      }
+      
+      let response = await this.canvasClient.connectWallet(chainId);
+      if (!response?.untrusted.success) {
+        throw new Error('Failed to connect wallet');
+      }
+      return response.untrusted.address;
     } catch (error) {
       console.error("Connection error:", error);
       return null;
     }
   };
 
+  base64tobase58 = (base64: string) => {
+    return base58.encode(Buffer.from(base64, 'base64'));
+  }
+
   signTransaction = async (_tx: string, _context: ActionContext) => {
+    console.log('signTransaction');
     try {
-      const signature = "";
-      return { signature };
+      console.log('signTransaction', _tx);
+      const results = await this.canvasClient.signAndSendTransaction({
+        unsignedTx: this.base64tobase58(_tx),
+        awaitCommitment: 'confirmed',
+        chainId: chainId,
+      });
+
+      if (!results?.untrusted.success) {
+        throw new Error('Failed to sign transaction');
+      }
+      return {signature: results.untrusted.signedTx};
     } catch (error) {
       console.error("Transaction signing error:", error);
       return { error: "Failed to sign transaction" };
@@ -30,6 +66,7 @@ class MyActionAdapter implements ActionAdapter {
   };
 
   confirmTransaction = async (_signature: string, _context: ActionContext) => {
+    console.log('confirmTransaction');
     try {
       await true;
     } catch (error) {
@@ -38,11 +75,15 @@ class MyActionAdapter implements ActionAdapter {
   };
 }
 
+
+
 const App = () => {
   //const [count, setCount] = useState(0);
   const [action, setAction] = useState<Action | null>(null);
 
+
   useEffect(() => {
+
     const fetchAction = async () => {
       const action = await Action.fetch(
         "https://blink-chat.xyz/api/actions/chat",
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import csp from 'vite-plugin-csp';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+      },
+      protocolImports: true,
+    }),
+  
+  ],
+  build: {
+    // Ensure the build does not use inline scripts
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: false
+      }
+    }
+  },
+  server: {
+    hmr: false, // Disable HMR to avoid inline script injection
+    headers: {
+      'Content-Security-Policy': "connect-src https://*.dial.to/ https://proxy.dial.to/; img-src https://*.dial.to/ https://proxy.dial.to/;"
+    }
+  },
 })
