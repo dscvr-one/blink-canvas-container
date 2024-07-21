598f4f09e44f494df5c75ec55073351f96833905
Rick Porter <rick@dscvr.one>
2024-07-21 08:20:48 -0700

    fix: move adatper, set url based on query params

--- src/App.tsx
+++ src/App.tsx
@@ -2,94 +2,34 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import {
   Action,
-  type ActionAdapter,
   ActionContainer,
-  ActionContext,
 } from "@dialectlabs/blinks";
 import '@dialectlabs/blinks/index.css';
 import './blink.css'
-import { CanvasClient } from '@dscvr-one/canvas-client-sdk';
-import * as base58 from 'bs58';
-
-
-const chainId = 'solana:101';
-
-class MyActionAdapter implements ActionAdapter {
-  canvasClient: CanvasClient;
-  constructor() {
-    this.canvasClient = new CanvasClient();
-    this.canvasClient.ready().then(() => {
-      console.log('Canvas client ready');
-    });
-
-  }
-
-  connect = async (_context: ActionContext) => {
-    try {
-      if (!this.canvasClient) {
-        throw new Error('Canvas client not initialized');
-      }
-      
-      let response = await this.canvasClient.connectWallet(chainId);
-      if (!response?.untrusted.success) {
-        throw new Error('Failed to connect wallet');
-      }
-      return response.untrusted.address;
-    } catch (error) {
-      console.error("Connection error:", error);
-      return null;
-    }
-  };
-
-  base64tobase58 = (base64: string) => {
-    return base58.encode(Buffer.from(base64, 'base64'));
-  }
-
-  signTransaction = async (_tx: string, _context: ActionContext) => {
-    console.log('signTransaction');
-    try {
-      console.log('signTransaction', _tx);
-      const results = await this.canvasClient.signAndSendTransaction({
-        unsignedTx: this.base64tobase58(_tx),
-        awaitCommitment: 'confirmed',
-        chainId: chainId,
-      });
-
-      if (!results?.untrusted.success) {
-        throw new Error('Failed to sign transaction');
-      }
-      return {signature: results.untrusted.signedTx};
-    } catch (error) {
-      console.error("Transaction signing error:", error);
-      return { error: "Failed to sign transaction" };
-    }
-  };
-
-  confirmTransaction = async (_signature: string, _context: ActionContext) => {
-    console.log('confirmTransaction');
-    try {
-      await true;
-    } catch (error) {
-      console.error("Transaction confirmation error:", error);
-    }
-  };
-}
-
-
+import { CanvasAdapter, isIframe } from "./canvas-adapter";
 
 const App = () => {
   //const [count, setCount] = useState(0);
   const [action, setAction] = useState<Action | null>(null);
 
-
   useEffect(() => {
+    const adapter = isIframe() ? new CanvasAdapter() : undefined;
 
     const fetchAction = async () => {
-      const action = await Action.fetch(
-        "https://blink-chat.xyz/api/actions/chat",
-        new MyActionAdapter()
-      );
-      setAction(action);
+      // Get the current URL
+      const url = new URL(window.location.href);
+      // Get the 'action' query parameter
+      const actionParam = url.searchParams.get('action') ?? 'https://blink-chat.xyz/api/actions/chat';
+      
+      if (actionParam) {
+        const action = await Action.fetch(
+          actionParam,
+          adapter
+        );
+        setAction(action);
+      } else {
+        console.error("No action parameter provided in URL");
+      }
     };
     fetchAction();
   }, []);
@@ -108,7 +48,7 @@ const App = () => {
         <ActionContainer
           action={action}
           websiteUrl="https://example.com"
-          websiteText="Example Website"
+          websiteText=""
           callbacks={exampleCallbacks}
           securityLevel={exampleSecurityLevel}
           stylePreset="x-dark"
