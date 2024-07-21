130c3af38e1b7596557db8eeb5813b2d63ebed48
Rick Porter <rick@dscvr.one>
2024-07-20 20:04:54 -0700

    add to connect-src

--- src/App.tsx
+++ src/App.tsx
@@ -74,17 +74,6 @@ const App = () => {
           stylePreset="x-dark"
         />
       )}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
