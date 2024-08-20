52f059b3781f285385c4983d822d79c459080ddf
Jimmy Madrigal <jimmymadrigal+github@gmail.com>
2024-08-20 17:19:17 -0600

    feat: use canvas wallet adapter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  WalletProvider,
+  ConnectionProvider,
+} from '@solana/wallet-adapter-react';
+import App from './App.tsx';
+import './index.css';
+
+const RPC_ENDPOINT = 'https://api.mainnet-beta.solana.com';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WalletProvider wallets={[]}>
+      <ConnectionProvider endpoint={RPC_ENDPOINT}>
+        <App />
+      </ConnectionProvider>
+    </WalletProvider>
+  </React.StrictMode>
+);
